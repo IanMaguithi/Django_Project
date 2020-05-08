098d69b7e53b0abc3145b9eb98f39f253913ebaf
IanMaguithi <maguithian13427@gmail.com>
2020-05-08 13:13:59 +0300

    added links to news, about us and contact

--- travello/urls.py
+++ travello/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('destinations/', views.destinations, name='destination'),
+    path('contact/', views.contact, name='contact'),
+    path('news/', views.news, name='news'),
+    path('elements', views.elements, name='elements')
 ]
--- travello/views.py
+++ travello/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'travello/index.html')
+
+
+def about(request):
+    return render(request, 'travello/about.html')
+
+
+def destinations(request):
+    return render(request, 'travello/destinations.html')
+
+
+def contact(request):
+    return render(request, 'travello/contact.html')
+
+
+def news(request):
+    return render(request, 'travello/news.html')
+
+
+def elements(request):
+    return render(request, 'travello/elements.html')
