22fae40778b90c17b510b737798d4cf11ad925c8
IanMaguithi <maguithian13427@gmail.com>
2020-05-09 12:55:11 +0300

    made a migration of a model destinations

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Destinations(models.Model):
+    name = models.CharField(max_length=90)
+    image = models.ImageField(upload_to='pics')
+    description = models.TextField()
+    price = models.IntegerField(default=0)
+    offer = models.BooleanField(default=False)
